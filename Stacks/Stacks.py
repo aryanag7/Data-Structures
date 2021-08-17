a5d781af451e0b6534112b48ffd831afc21ac101
aryanag7 <62555290+aryanag7@users.noreply.github.com>
2021-08-17 17:38:55 +0530

    Update Stacks.py
    new stack

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -1,1 +1,32 @@
 
+#Stack using array
+class Stack:
+    def __init__(self):
+        self.__mystack=[]
+    def push(self,val):
+        self.__mystack.append(val)
+    def pop(self):
+        if self.isempty():
+            return "stack underflow"
+        self.__mystack.pop()
+        
+    def top(self):
+        return self.__mystack[-1]
+    def isempty(self):
+        if self.__mystack==[]:
+            return True
+        else:
+            return False
+    def printele(self):
+        for i in self.__mystack:
+            print(i,end=" ")
+        return 
+s=Stack()
+s.push(12)
+s.push(21)
+s.push(344)
+s.push(4)
+s.push(77)
+s.push(646)
+s.printele()
+
