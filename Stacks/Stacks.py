847cdd6bae0c0295ff525fb9e912aac72a9fa384
aryanag7 <62555290+aryanag7@users.noreply.github.com>
2021-08-18 16:24:34 +0530

    Update Stacks.py
    commit

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -3,20 +3,29 @@
 class Stack:
     def __init__(self):
         self.__mystack=[]
+        
+#     to push an element into stack    
+
     def push(self,val):
         self.__mystack.append(val)
+#      to pop an element from the stack   
+
     def pop(self):
         if self.isempty():
             return "stack underflow"
         self.__mystack.pop()
-        
+#      to return the element at top
+
     def top(self):
         return self.__mystack[-1]
+    
     def isempty(self):
         if self.__mystack==[]:
             return True
         else:
             return False
+#      to print the elements of stack
+
     def printele(self):
         for i in self.__mystack:
             print(i,end=" ")
